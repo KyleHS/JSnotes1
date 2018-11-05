422c612d13b384e5cd24e8a2595349cfd979b89e
Kyle <k15156@yahoo.com>
2018-11-05 10:40:04 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+var userName = prompt('What is your name?');  /*Ask user for input*/
+alert("Nice to meet you, " + userName);   /*popup with message*/
+console.log("Also great to meet you, " + userName); /*Logs directly to console*/
